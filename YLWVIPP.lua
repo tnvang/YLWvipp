6eaa5b7997bdd797560ac903f96f3f2cdb6a8f71
tnvang <tnvang69211@gmail.com>
2026-02-27 19:34:31 +0700

    Create YLWVIPP.lua

---
+++ YLWVIPP.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "YLWsieuvippp" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ylwyeuem/ylwscrvipp/refs/heads/main/BloxFruitPVP.lua"))()
