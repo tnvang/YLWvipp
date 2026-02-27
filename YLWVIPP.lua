73b8d4ba69a8f27d9eff1b2f380a4d3ad89267d6
Ylwvipp <tnvang69211@gmail.com>
2026-02-27 20:07:01 +0700

    YLWVIPP.lua

--- YLWVIPP.lua
+++
@@ -1,3 +0,0 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Key = "YLWsieuvippp" 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ylwyeuem/ylwscrvipp/refs/heads/main/BloxFruitPVP.lua"))()
